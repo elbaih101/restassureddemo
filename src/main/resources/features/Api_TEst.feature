1a0a73a634063824155e961225c2e9eb7c67753e
elbaih <l.ofwarhd@gmail.com>
2023-07-06 17:40:57 +0300

    agter asserting the body

--- src/main/resources/features/Api_TEst.feature
+++ src/main/resources/features/Api_TEst.feature
@@ -1,4 +1,13 @@
 Feature: Api TEst
-  Scenario: client request for the us time zone
-    When requestin the get request "http://zippopotam.us/us/90210"
+  Scenario Outline: client request for the us time zone
+    When requestin the get request with "<country>" "<zipcode>"
+
     Then the response must return code 200 andcontent type of "json"
+    Then the check the response body for the place to be "<place>"
+#    Then  check reponse body fore the state to be "California"
+#    Then check whether the collection places has item "CA"
+    Examples:
+      |country|zipcode|place|
+      |us     |90210  |Beverly Hills|
+      |us     |12345  |Schenectady|
+      |ca     |B2R    |Waverley|
--- src/test/java/com/elbaih/stepDefs/Api_Test.java
+++ src/test/java/com/elbaih/stepDefs/Api_Test.java
@@ -2,26 +2,49 @@ package com.elbaih.stepDefs;
 
 import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
-import io.restassured.RestAssured;
+
 import io.restassured.http.*;
+import static org.hamcrest.Matchers.*;
 import io.restassured.response.Response;
-import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import static io.restassured.RestAssured.*;
 public class Api_Test {
     Response response;
    SoftAssert assrt =new SoftAssert();
+    private static final String BASE_URL = "http://zippopotam.us";
     @When("requestin the get request {string}")
-    public void requestinTheGetRequest(String arg0) {
-      response= given().log().all().when().
-              get(arg0);
+
+    @When("requestin the get request with {string} {string}")
+    public void requestinTheGetRequestWith(String arg0, String arg1) {
+
+        response= given()./*pathParam("countrycode",arg0).pathParam("zipcode",arg1).*/log().all().when().
+                get(BASE_URL+"/"+arg0+"/"+arg1);
     }
+
 
 
 
     @Then("the response must return code {int} andcontent type of {string}")
     public void theResponseMustReturnCodeAndcontentTypeOf(int code, String arg1) {
         response.then().assertThat().statusCode(code).assertThat().contentType(ContentType.JSON);
+        response.then().log().body();
 
     }
+
+    @Then("the check the response body for the place to be {string}")
+    public void theCheckTheResponseBodyForThePlaceToBe(String arg0) {
+        response.then().assertThat().body("places[0].'place name'",equalTo(arg0));
+    }
+
+    @Then("check reponse body fore the state to be {string}")
+    public void checkReponseBodyForeTheStateToBe(String arg0) {
+        response.then().assertThat().body("places[0].state",equalTo(arg0));
+    }
+
+    @Then("check whether the collection places has item {string}")
+    public void checkWhetherTheCollectionPlacesHasItem(String arg0) {
+        response.then().assertThat().body("places.'state abbreviation'",hasItem(arg0));
+    }
+
+
 }
